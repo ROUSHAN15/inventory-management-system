ae86265a9a4485b71552e9a3fa9b9c665c56a9af
Roushan Kumar Verma <roushankkverma@gmail.com>
2025-11-19 01:14:39 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import connectDB from './db/connection.js';
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT,()=>{
+    connectDB();
+    console.log('server is running at localhost 3000');
+})
